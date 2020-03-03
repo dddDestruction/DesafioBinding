c2b6b0199cb8d1dfce92285c469b35fcf3857625
Nicolas <nifradich@hotmail.com>
2020-03-03 13:13:03 -0300

    Estilo implementado

--- app/src/main/java/cl/desafiolatam/desafiobinding/MainActivity.java
+++ app/src/main/java/cl/desafiolatam/desafiobinding/MainActivity.java
@@ -13,6 +13,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         ActivityMainBinding binding= DataBindingUtil.setContentView(this, R.layout.activity_main);
-        binding.textView.setText("Welcome again!");
+
     }
 }
